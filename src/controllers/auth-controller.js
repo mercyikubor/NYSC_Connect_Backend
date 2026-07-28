0be667a4d0e35da053d8a60a2895ac22d5a8ea3b
M863-ik <ikubormercy373@gmail.com>
2026-07-28 21:01:30 +0100

    add corps member registration flow

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,22 @@
+import { registerCorpsMember } from "../services/auth-services.js";
+import { validationResult } from "express-validator";
+
+export const registerCorpsMemberController = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array(),
+    });
+    try {
+      const result = awaitRegisterCorpsMember(req.body);
+
+      return res.status(201).json(result);
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
+};
