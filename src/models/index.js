8678d7f2288601df5f92f5f1f231623289421d4e
Ikeh Kenechukwu <ikehkenechukwu4@gmail.com>
2026-08-03 22:06:42 +0100

    Fix: Removed the business logic in models/index.js file

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,6 @@
 import sequelize from "../config/db.js";
 import User from "./user.js";
 import CorpsMember from "./corpsMember.js";
-import Business from "./business.js";
 
 // CorpMember Association (One-to-One)
 User.hasOne(CorpsMember, {
@@ -13,14 +12,5 @@ CorpsMember.belongsTo(User, {
   as: "user",
 });
 
-// Business Association (One-to-One)
-User.hasOne(Business, {
-  foreignKey: "sellerId",
-});
 
-Business.belongsTo(User, {
-  foreignKey: "sellerId",
-  as: "user",
-});
-
-export { sequelize, User, CorpsMember, Business };
+export { sequelize, User, CorpsMember };
