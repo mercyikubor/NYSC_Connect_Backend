6fb189761eb7eea4305e4f03876896087f407155
M863-ik <ikubormercy373@gmail.com>
2026-08-04 11:09:27 +0100

    updated index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,15 @@
 import sequelize from "../config/db.js";
 import User from "./user.js";
 import CorpsMember from "./corpsMember.js";
-import Business from "./business.js";
+import State from "./state.js";
+import LGA from "./lga.js";
+import Landlord from "./landlord.js";
+import Property from "./property.js";
 
-// CorpMember Association (One-to-One)
+// User -> CorpsMember Association (One-to-One)
 User.hasOne(CorpsMember, {
   foreignKey: "userId",
+  as: "corpsMember",
 });
 
 CorpsMember.belongsTo(User, {
@@ -13,14 +17,49 @@ CorpsMember.belongsTo(User, {
   as: "user",
 });
 
-// Business Association (One-to-One)
-User.hasOne(Business, {
-  foreignKey: "userId",
+// State -> LGA Association (One-to-Many)
+State.hasMany(LGA, {
+  foreignKey: "stateId",
+  as: "lgas",
 });
 
-Business.belongsTo(User, {
-  foreignKey: "userId",
-  as: "user",
+LGA.belongsTo(State, {
+  foreignKey: "stateId",
+  as: "state",
 });
 
-export { sequelize, User, CorpsMember, Business };
+// State -> CorpsMember Association (One-to-Many)
+State.hasMany(CorpsMember, {
+  foreignKey: "stateId",
+  as: "corpsMembers",
+});
+CorpsMember.belongsTo(State, {
+  foreignKey: "stateId",
+  as: "state",
+});
+
+// LGA -> CorpsMember Association (One-to-Many)
+LGA.hasMany(CorpsMember, {
+  foreignKey: "lgaId",
+  as: "corpsMembers",
+});
+CorpsMember.belongsTo(LGA, {
+  foreignKey: "lgaId",
+  as: "lga",
+});
+
+export { sequelize, User, CorpsMember, State, LGA };
+
+// Landlord -> Property Association (One-to-Many)
+Landlord.hasMany(Property, {
+  foreignKey: "landlordId",
+  as: "properties",
+  onDelete: "CASCADE",
+});
+
+Property.belongsTo(Landlord, {
+  foreignKey: "landlordId",
+  as: "landlord",
+});
+
+export { sequelize, User, CorpsMember, Landlord, Property };
