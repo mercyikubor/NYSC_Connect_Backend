ab1d539cfdb0b2ce4388fbd9c9d6fe040adda61d
M863-ik <ikubormercy373@gmail.com>
2026-08-05 17:21:50 +0100

    updated uploads

--- src/uploads/file-uploads.js
+++ src/uploads/file-uploads.js
@@ -11,15 +11,7 @@ const storage = multer.diskStorage({
   },
 });
 
-<<<<<<< HEAD
 export const uploadCallUpLetter = multer({
   storage,
   limits: { fileSize: 5 * 1024 * 1024 },
 });
-=======
-const upload = multer({ storage });
-
-export const uploadCallUpLetter = upload.single("callUpLetter");
-
-export default upload;
->>>>>>> 8ce52f178994cc9a96a2a9ead2b71e08c3a49a8f
